689138e347e494d7ca00d96102ec3013b7c9409a
jackopuff <jackson@carterrealm.com>
2017-06-01 12:31:34 -0600

    added paralleling background

--- SpaghettiSurfer/Controller/SpaghettiSurfer.cs
+++ SpaghettiSurfer/Controller/SpaghettiSurfer.cs
@@ -3,6 +3,7 @@ using SpaghettiSurfer.Model;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SpaghettiSurfer.View;
 
 namespace SpaghettiSurfer.Controller
 {
@@ -14,6 +15,16 @@ namespace SpaghettiSurfer.Controller
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 		private Player player;
+		// Keyboard states used to determine key presses
+		private KeyboardState currentKeyboardState;
+		private KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		private GamePadState currentGamePadState;
+		private GamePadState previousGamePadState;
+
+		// A movement speed for the player
+		private float playerMoveSpeed;
 		public Game1()
 		{
 			graphics = new GraphicsDeviceManager(this);
@@ -33,6 +44,8 @@ namespace SpaghettiSurfer.Controller
 			// TODO: Add your initialization logic here
 
 			base.Initialize();
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
 		}
 
 		/// <summary>
@@ -41,13 +54,13 @@ namespace SpaghettiSurfer.Controller
 		/// </summary>
 		protected override void LoadContent()
 		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch(GraphicsDevice);
-			// Load the player resources 
-			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("Animation/shipAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
 
-			player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
-			//TODO: use this.Content to load your game content here 
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
 		}
 
 		/// <summary>
@@ -65,7 +78,17 @@ namespace SpaghettiSurfer.Controller
 #endif
 
 			// TODO: Add your update logic here
+			// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
 
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+			//Update the player
+			UpdatePlayer(gameTime);
 			base.Update(gameTime);
 		}
 
@@ -85,6 +108,35 @@ namespace SpaghettiSurfer.Controller
 			//TODO: Add your drawing code here
 
 			base.Draw(gameTime);
+		}
+		private void UpdatePlayer(GameTime gameTime)
+		{
+			player.Update(gameTime);
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
 		}
 	}
 }
--- SpaghettiSurfer/Model/Player.cs
+++ SpaghettiSurfer/Model/Player.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-
+using SpaghettiSurfer.View;
 namespace SpaghettiSurfer.Model
 {
 	public class Player
 	{
 		// Animation representing the player
-		private Texture2D playerTexture;
-		public Texture2D PlayerTexture
+		private Animation playerAnimation;
+		public Animation PlayerAnimation
 		{
-			get { return playerTexture; }
-			set { playerTexture = value; }
+			get { return playerAnimation; }
+			set { playerAnimation = value; }
 		}
 
 		// Position of the Player relative to the upper left side of the screen
@@ -20,7 +20,7 @@ namespace SpaghettiSurfer.Model
 
 		// State of the player
 		private bool active;
-public bool Active
+		public bool Active
 		{
 			get { return active; }
 			set { active = value; }
@@ -37,35 +37,40 @@ public bool Active
 		// Get the width of the player ship
 		public int Width
 		{
-			get { return PlayerTexture.Width; }
+			get { return playerAnimation.FrameWidth; }
 		}
 
 		// Get the height of the player ship
 		public int Height
 		{
-			get { return PlayerTexture.Height; }
+			get { return playerAnimation.FrameHeight; }
 		}
-		public void Initialize(Texture2D texture, Vector2 position)
+		// Initialize the player
+		public void Initialize(Animation animation, Vector2 position)
 		{
-			PlayerTexture = texture;
+			playerAnimation = animation;
 
-			// Set the starting position of the player around the middle of the screen and to the back 
+			// Set the starting position of the player around the middle of the screen and to the back
 			Position = position;
 
-			// Set the player to be active 
+			// Set the player to be active
 			Active = true;
 
 			// Set the player health
 			Health = 100;
 		}
 
-		public void Update()
+		// Update the player animation
+		public void Update(GameTime gameTime)
 		{
+			playerAnimation.Position = Position;
+			playerAnimation.Update(gameTime);
 		}
 
+		// Draw the player
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			playerAnimation.Draw(spriteBatch);
 		}
 	}
 }
